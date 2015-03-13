c0c127d0658cf411a6d4967f3a42b189c5fe3914
DCubix <diego95lopes@live.com>
2015-03-13 16:01:39 -0300

    Added test data

--- Debug/test.lua
+++ Debug/test.lua
@@ -2,37 +2,13 @@ screen(320, 240, 32, "Test")
 lighting(true)
 
 local key = 0
-local tset_img = image("tiles.png")
+local tset_img = image("data/tile1.png")
 local tset = tileset(tset_img, 3, 6, "abcdefghijklmnopqr")
-
-local l5 = light(160, 120, 1.0, 1.0, 1.0)
-local l4 = light(170, 150, 1.0, 0.7, 0.5)
-local l3 = light(80, 90, 1.0, 1.0, 1.0)
-local ocs = {
-	boxoccluder(50, 100, 15, 20),
-	boxoccluder(70, 100, 15, 20),
-	boxoccluder(100, 100, 15, 20),
-	boxoccluder(130, 100, 15, 20),
-	boxoccluder(160, 100, 15, 20)
-}
-l3.intensity = 1.0
-l3.radius = 100.0
-l4.intensity = 1.0
-l4.radius = 120.0
-l5.intensity = 0.6
-l5.radius = 120.0
-
-addlight("l5", l5)
-addlight("l4", l4)
-addlight("l3", l3)
-
-for i = 1, #ocs do
-	addoccluder("oc"..i, ocs[i])
-end
 
 map = {
 	{"p","g","g","g","g","o"},
 	{"q","d","c","d","d","r"},
+	{"q","d","d","d","d","r"},
 	{"q","d","d","d","d","r"}
 }
 
@@ -43,8 +19,12 @@ while key ~= 27 do
 	
 	for i = 1, #map do
 		for j = 1, #map[1] do
-			blit(tset:tile(map[i][j]), (j-1)*64, (i-1)*64)
+			blit(tset:tile(map[i][j]), (j-1)*16, (i-1)*16)
 		end
+	end
+	
+	for i = 1, #ocs do
+		rect(ocs[i].x, ocs[i].y, ocs[i].width, ocs[i].height, true)
 	end
 	
 	l5.x = mousex()
